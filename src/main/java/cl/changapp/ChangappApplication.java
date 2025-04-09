4dfcc320ccdc55451af44ec7daebb2ef0ea4a6e0
unknown <rodolfosotofigueroa95@gmail.com>
2025-04-09 16:59:34 -0400

    safety commit

---
+++ src/main/java/cl/changapp/ChangappApplication.java
@@ -0,0 +1,13 @@
+package cl.changapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/changapp/ChangappApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.changapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
